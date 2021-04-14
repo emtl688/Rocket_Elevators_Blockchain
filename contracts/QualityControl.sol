6483f67bc071530b8bc2140d6f75350be0fa08e4
emmatshi-stack <tshibangubiayi@gmail.com>
2021-04-14 16:38:19 -0400

    debutfixqualitycontrol

--- contracts/QualityControl.sol
+++ contracts/QualityControl.sol
@@ -1,59 +1,29 @@
-//SPDX-License-Identifier: MIT
-pragma solidity >=0.4.22 <0.8.0;
+pragma solidity ^0.5.0;
 pragma experimental ABIEncoderV2;
 
-import './SolutionManufacturing.sol';
+
 
 contract QualityControl {
-// peermit object
-    struct Permit{
-        uint256 identifier;
-        uint256 startDate;
-        uint256 endDate;
-        //uint datetime // 1512220800
-    }
-// product object
-    struct Product{
-        string name;
-        uint64 quantity;
-        bool verified;
-    }
+ 
 
-    struct Verification{
-        Permit certificate;
-        Permit perm;
-        Product[] mList;
-    }
-    //Product[] mList;
-    Verification v;
-    uint64 index = 0;
+    struct test{
+        bool Door;
+        bool Cable;
+        bool  Brake;
+        bool perm;
+        bool Certificate;
 
-// filling the 
-    function createPermit() private view returns(Permit memory certificate){
-        Permit memory p;
-        p.identifier = block.timestamp/2;
-        p.startdate = block.timestamp;
-        p.enddate = block.timestamp + (10* 365 days);
-        return p;
     }
-
-    function VerificationStamp(address a) public{
-        v.certificate = createPermit();
-        v.perm = createPermit();
-        SolutionManufacturing.Product[] memory products = SolutionManufacturing(a).viewOrder();
-        for(index;index<products.length;index++){
-            Product memory p;
-            p.verified = true;
-            p.name = products[index].Name;
-            p.quantity = products[index].Quantity;
-            v.mList.push(p);
+   
+    
+    function validateUnit(bool cabletest, bool braketest,bool doortest, bool perm, bool certificate) public returns (bool){
+        if (doortest == true && braketest == true && cabletest == true && perm == true  && certificate == true  ){
+            return true;
+        }
+        else {
+            return false;
         }
     }
-
-    function getVerification() public view returns(Verification memory){
-        return v;
-    }
-
 
 
 }
