7fe7e4891952db9c5f628d9fb83bc2099e90eaf8
adriengobeil3 <adirengobeil3@gmail.com>
2021-04-14 09:28:15 -0400

    get by id instead of index

--- contracts/Manufacturing.sol
+++ contracts/Manufacturing.sol
@@ -3,6 +3,7 @@ pragma solidity ^0.5.0;
 contract Manufacturing {
 
     struct Door {
+        uint id;
         address manufacturerAddress;
         uint nbOfAluminumBars;
         uint nbOfStainlessSheets;
@@ -13,6 +14,7 @@ contract Manufacturing {
     }
     
     struct Controller {
+        uint id;
         address manufacturerAddress;
         uint nbOfAluminumBars;
         uint nbOfStainlessSheets;
@@ -23,6 +25,7 @@ contract Manufacturing {
     }
     
     struct ControlPanel {
+        uint id;
         address manufacturerAddress;
         uint nbOfAluminumBars;
         uint nbOfStainlessSheets;
@@ -33,6 +36,7 @@ contract Manufacturing {
     }
     
     struct CallSign {
+        uint id;    
         address manufacturerAddress;
         uint nbOfAluminumBars;
         uint nbOfStainlessSheets;
@@ -46,8 +50,14 @@ contract Manufacturing {
     ControlPanel[] public controlPanels;
     CallSign[] public callSigns;
 
+    uint nextDoorId = 1;
+    uint nextControllerId = 1;
+    uint nextControlPanelId = 1;
+    uint nextCallSignId = 1;
+
     function addDoor(uint nbOfAluminumBars, uint nbOfStainlessSheets, uint nbOfOtherHardware, uint nbOfBumpers, uint nbOfLightBulbs, uint nbOfLeds) public {
         doors.length++;
+        doors[doors.length-1].id = nextDoorId;
         doors[doors.length-1].manufacturerAddress = msg.sender;
         doors[doors.length-1].nbOfAluminumBars = nbOfAluminumBars;
         doors[doors.length-1].nbOfStainlessSheets = nbOfStainlessSheets;
@@ -55,9 +65,11 @@ contract Manufacturing {
         doors[doors.length-1].nbOfBumpers = nbOfBumpers;
         doors[doors.length-1].nbOfLightBulbs = nbOfLightBulbs;
         doors[doors.length-1].nbOfLeds = nbOfLeds;
+        nextDoorId++;
     }
         function addController(uint nbOfAluminumBars, uint nbOfStainlessSheets, uint nbOfOtherHardware, uint nbOfBumpers, uint nbOfLightBulbs, uint nbOfLeds) public {
         controllers.length++;
+        controllers[controllers.length-1].id = nextControllerId;
         controllers[controllers.length-1].manufacturerAddress = msg.sender;
         controllers[controllers.length-1].nbOfAluminumBars = nbOfAluminumBars;
         controllers[controllers.length-1].nbOfStainlessSheets = nbOfStainlessSheets;
@@ -65,10 +77,12 @@ contract Manufacturing {
         controllers[controllers.length-1].nbOfBumpers = nbOfBumpers;
         controllers[controllers.length-1].nbOfLightBulbs = nbOfLightBulbs;
         controllers[controllers.length-1].nbOfLeds = nbOfLeds;
+        nextControllerId++;
     }
     
     function addControlPanel(uint nbOfAluminumBars, uint nbOfStainlessSheets, uint nbOfOtherHardware, uint nbOfBumpers, uint nbOfLightBulbs, uint nbOfLeds) public {
         controlPanels.length++;
+        controlPanels[controlPanels.length-1].id = nextControlPanelId;
         controlPanels[controlPanels.length-1].manufacturerAddress = msg.sender;
         controlPanels[controlPanels.length-1].nbOfAluminumBars = nbOfAluminumBars;
         controlPanels[controlPanels.length-1].nbOfStainlessSheets = nbOfStainlessSheets;
@@ -76,10 +90,12 @@ contract Manufacturing {
         controlPanels[controlPanels.length-1].nbOfBumpers = nbOfBumpers;
         controlPanels[controlPanels.length-1].nbOfLightBulbs = nbOfLightBulbs;
         controlPanels[controlPanels.length-1].nbOfLeds = nbOfLeds;
+        nextControlPanelId++;
     }
     
     function addCallSign(uint nbOfAluminumBars, uint nbOfStainlessSheets, uint nbOfOtherHardware, uint nbOfBumpers, uint nbOfLightBulbs, uint nbOfLeds) public {
         callSigns.length++;
+        callSigns[callSigns.length-1].id = nextCallSignId;
         callSigns[callSigns.length-1].manufacturerAddress = msg.sender;
         callSigns[callSigns.length-1].nbOfAluminumBars = nbOfAluminumBars;
         callSigns[callSigns.length-1].nbOfStainlessSheets = nbOfStainlessSheets;
@@ -87,20 +103,42 @@ contract Manufacturing {
         callSigns[callSigns.length-1].nbOfBumpers = nbOfBumpers;
         callSigns[callSigns.length-1].nbOfLightBulbs = nbOfLightBulbs;
         callSigns[callSigns.length-1].nbOfLeds = nbOfLeds;
+        nextCallSignId++;
     }
 
-    function getDoor(uint index) public view returns (uint, uint, uint, uint, uint, uint) {
-        return (doors[index].nbOfAluminumBars, doors[index].nbOfStainlessSheets, doors[index].nbOfOtherHardware,
-        doors[index].nbOfBumpers, doors[index].nbOfLightBulbs, doors[index].nbOfLeds);
+    function getDoor(uint id) public view returns (uint, uint, uint, uint, uint, uint) {
+        for (uint i=0; i<doors.length; i++) {
+            if (doors[i].id == id) {
+                return (doors[i].nbOfAluminumBars, doors[i].nbOfStainlessSheets, doors[i].nbOfOtherHardware,
+                doors[i].nbOfBumpers, doors[i].nbOfLightBulbs, doors[i].nbOfLeds);           
+            }
+        }
     }
     
-    function getController(uint index) public view returns (uint, uint, uint, uint, uint, uint) {
-        return (controllers[index].nbOfAluminumBars, controllers[index].nbOfStainlessSheets, controllers[index].nbOfOtherHardware,
-        controllers[index].nbOfBumpers, controllers[index].nbOfLightBulbs, controllers[index].nbOfLeds);
+    function getController(uint id) public view returns (uint, uint, uint, uint, uint, uint) {
+        for (uint i=0; i<controllers.length; i++) {
+            if (controllers[i].id == id) {
+                return (controllers[i].nbOfAluminumBars, controllers[i].nbOfStainlessSheets, controllers[i].nbOfOtherHardware,
+                controllers[i].nbOfBumpers, controllers[i].nbOfLightBulbs, controllers[i].nbOfLeds);           
+            }
+        }
     }
     
-    function getCallSign(uint index) public view returns (uint, uint, uint, uint, uint, uint) {
-        return (callSigns[index].nbOfAluminumBars, callSigns[index].nbOfStainlessSheets, callSigns[index].nbOfOtherHardware,
-        callSigns[index].nbOfBumpers, callSigns[index].nbOfLightBulbs, callSigns[index].nbOfLeds);
+    function getControlPanel(uint id) public view returns (uint, uint, uint, uint, uint, uint) {
+        for (uint i=0; i<controlPanels.length; i++) {
+            if (controlPanels[i].id == id) {
+                return (controlPanels[i].nbOfAluminumBars, controlPanels[i].nbOfStainlessSheets, controlPanels[i].nbOfOtherHardware,
+                controlPanels[i].nbOfBumpers, controlPanels[i].nbOfLightBulbs, controlPanels[i].nbOfLeds);           
+            }
+        }
+    }
+    
+    function getCallSign(uint id) public view returns (uint, uint, uint, uint, uint, uint) {
+        for (uint i=0; i<callSigns.length; i++) {
+            if (callSigns[i].id == id) {
+                return (callSigns[i].nbOfAluminumBars, callSigns[i].nbOfStainlessSheets, callSigns[i].nbOfOtherHardware,
+                callSigns[i].nbOfBumpers, callSigns[i].nbOfLightBulbs, callSigns[i].nbOfLeds);           
+            }
+        }
     }
 }
