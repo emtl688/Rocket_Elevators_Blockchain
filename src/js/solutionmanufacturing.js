ca03ca298fa70ee5cd569fe2c4b83cafdb71ed60
emtl688 <ericpmoran1@gmail.com>
2021-04-16 11:02:25 -0400

    Manufacturing testing

--- src/js/solutionmanufacturing.js
+++ src/js/solutionmanufacturing.js
@@ -4,98 +4,78 @@ App = {
 
   initWeb3: async function() {
     // Modern dapp browsers...
-if (window.ethereum) {
-  App.web3Provider = window.ethereum;
-  try {
-    // Request account access
-    await window.ethereum.enable();
-  } catch (error) {
-    // User denied account access...
-    console.error("User denied account access")
-  }
-}
-// Legacy dapp browsers...
-else if (window.web3) {
-  App.web3Provider = window.web3.currentProvider;
-}
-// If no injected web3 instance is detected, fall back to Ganache
-else {
-  App.web3Provider = new Web3.providers.HttpProvider('http://localhost:7545');
-}
-web3 = new Web3(App.web3Provider);
-
+    if (window.ethereum) {
+      App.web3Provider = window.ethereum;
+      try {
+        // Request account access
+        await window.ethereum.enable();
+      } catch (error) {
+        // User denied account access...
+        console.error("User denied account access")
+      }
+    }
+    // Legacy dapp browsers...
+    else if (window.web3) {
+      App.web3Provider = window.web3.currentProvider;
+    }
+    // If no injected web3 instance is detected, fall back to Ganache
+    else {
+      App.web3Provider = new Web3.providers.HttpProvider('http://localhost:7545');
+    }
+    web3 = new Web3(App.web3Provider);
     return App.initContract();
   },
 
   initContract: function() {
-    $.getJSON('Adoption.json', function(data) {
+    $.getJSON('SolutionManufacturing.json', function(data) {
       // Get the necessary contract artifact file and instantiate it with @truffle/contract
-      var AdoptionArtifact = data;
-      App.contracts.Adoption = TruffleContract(AdoptionArtifact);
+      var SolutionManufacturingArtifact = data;
+      App.contracts.SolutionManufacturing = TruffleContract(SolutionManufacturingArtifact);
     
       // Set the provider for our contract
-      App.contracts.Adoption.setProvider(App.web3Provider);
+      App.contracts.SolutionManufacturing.setProvider(App.web3Provider);
     
       // Use our contract to retrieve and mark the adopted pets
-      return App.markAdopted();
+      // return App.markAdopted();
     });
 
     return App.bindEvents();
   },
 
   bindEvents: function() {
-    $(document).on('click', '.btn-adopt', App.handleAdopt);
+    $(document).on('click', '.btn-send-door', App.addDoor);
+    $(document).on('click', '.btn-send-controller', App.addController);
+    $(document).on('click', '.btn-send-control-panel', App.addControlPanel);
+    $(document).on('click', '.btn-send-call-sign', App.callSign);
   },
 
-  markAdopted: function() {
-    var adoptionInstance;
-
-App.contracts.Adoption.deployed().then(function(instance) {
-  adoptionInstance = instance;
-
-  return adoptionInstance.getAdopters.call();
-}).then(function(adopters) {
-  for (i = 0; i < adopters.length; i++) {
-    if (adopters[i] !== '0x0000000000000000000000000000000000000000') {
-      $('.panel-pet').eq(i).find('button').text('Success').attr('disabled', true);
-    }
-  }
-}).catch(function(err) {
-  console.log(err.message);
-});
-  },
-
-  handleAdopt: function(event) {
+  addDoor: function(event) {
     event.preventDefault();
 
-    var petId = parseInt($(event.target).data('id'));
+    var solutionManufacturingInstance;
 
-    var adoptionInstance;
+    web3.eth.getAccounts(function(error, accounts) {
+      if (error) {
+        console.log(error);
+      }
 
-web3.eth.getAccounts(function(error, accounts) {
-  if (error) {
-    console.log(error);
+      var account = accounts[0];
+
+      App.contracts.SolutionManufacturing.deployed().then(function(instance) {
+        solutionManufacturingInstance = instance;
+
+        console.log("Creating door...");
+
+        return solutionManufacturingInstance.addDoor(1, 2, 3, 4, 5, 6, {from: account});
+      }).catch(function(err) {
+        console.log(err.message);
+      });
+    });
   }
-
-  var account = accounts[0];
-
-  App.contracts.Adoption.deployed().then(function(instance) {
-    adoptionInstance = instance;
-
-    // Execute adopt as a transaction by sending account
-    return adoptionInstance.adopt(petId, {from: account});
-  }).then(function(result) {
-    return App.markAdopted();
-  }).catch(function(err) {
-    console.log(err.message);
-  });
-});
-  }
-
 };
 
 $(function() {
   $(window).load(function() {
-    App.init();
+    App.initWeb3();
   });
 });
