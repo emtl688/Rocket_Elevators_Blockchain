93fd6e2267ec6a1600876c6db2bec174f61d399c
emmatshi-stack <tshibangubiayi@gmail.com>
2021-04-16 02:28:23 -0400

    on my way

--- contracts/QualityControl.sol
+++ contracts/QualityControl.sol
@@ -1,21 +1,20 @@
 pragma solidity ^0.5.0;
+//pragma experimental ABIEncoderV2;
 
 contract QualityControl {
-<<<<<<< HEAD
+    address public customer;
  
-=======
->>>>>>> main
     struct Test {
         bool door;
         bool cable;
         bool brake;
-        string perm;
-        string certificate;
+        string battery;
+        string column;
+        
     }
-<<<<<<< HEAD
     
-    Test newTest = Test(false, false, false, "", "");
-   
+    
+   Test newTest = Test(true, true, true, "cccccc-55555-5555555-cc4c4c", "cccccc-55555-5555555-cc4c4c");
     function validateDoor(bool isPassed) public {
         newTest.door = isPassed;
     }
@@ -29,58 +28,30 @@ contract QualityControl {
     }
     
     function addPermit(string memory permitNumber) public {
-        newTest.perm = permitNumber;
+        newTest.column = permitNumber;
     }
     
     
     function addCertificate(string memory certificateNumber) public {
-        newTest.certificate = certificateNumber;
+        newTest.battery = certificateNumber;
     }
     
-    function getTest() public view returns (bool, bool, bool, string memory, string memory) {
-        return (newTest.door, newTest.cable, newTest.brake, newTest.perm, newTest.certificate);           
-=======
+    function getTest() public returns (bool) {
+            //Test memory p;
+            newTest.brake = true;
+            newTest.cable = true;
+            newTest.door = true;
+            newTest.column = "ghjkl";
+            newTest.battery = "ghjkl";
+          //print("a was submit is:", newTest.door);
 
-    Test newTest = Test(false, false, false, "", "");
-
-    function validateDoor(bool isPassed) public {
-        newTest.door = isPassed;
-    }
-
-    function validateCable(bool isPassed) public {
-        newTest.cable = isPassed;
-    }
-
-    function validateBrake(bool isPassed) public {
-        newTest.brake = isPassed;
-    }
-
-    function addPermit(string memory permitNumber) public {
-        newTest.perm = permitNumber;
-    }
-
-    function addCertificate(string memory certificateNumber) public {
-        newTest.certificate = certificateNumber;
-    }
-
-    function getTest()
-        public
-        view
-        returns (
-            bool,
-            bool,
-            bool,
-            string memory,
-            string memory
-        )
-    {
-        return (
-            newTest.door,
-            newTest.cable,
-            newTest.brake,
-            newTest.perm,
-            newTest.certificate
-        );
->>>>>>> main
+//          validateBrake(true);, bool, bool, string memory, string memory
+ //         validateDoor(true);(newTest.brake, newTest.cable, newTest.door, newTest.column, newTest.battery)
+  //        validateCable(true);
+   //       addPermit('cccccc-55555-5555555-cc4c4c4');
+    //      addCertificate('cccccc-55555-5555555-cc4c4c4');
+        
+        return true;           
     }
 }
+/// je charge mon phone##############################################
--- src/js/qualitycontrol.js
+++ src/js/qualitycontrol.js
@@ -1,68 +1,146 @@
 App = {
-  web3Provider: null,
-  contracts: {},
+    web3Provider: null,
+    doorV : false,
+    brakeV : false,
+    cableV : false,
+    batteryV : false,
+    columnV : false,
+    contracts: {},
 
-  initWeb3: async function() {
-    // Modern dapp browsers...
-if (window.ethereum) {
-  App.web3Provider = window.ethereum;
-  try {
-    // Request account access
-    await window.ethereum.enable();
-  } catch (error) {
-    // User denied account access...
-    console.error("User denied account access")
-  }
-}
-// Legacy dapp browsers...
-else if (window.web3) {
-  App.web3Provider = window.web3.currentProvider;
-}
-// If no injected web3 instance is detected, fall back to Ganache
-else {
-  App.web3Provider = new Web3.providers.HttpProvider('http://localhost:7545');
-}
-web3 = new Web3(App.web3Provider);
+    initWeb3: async function() {
+      // Modern dapp browsers...
+        if (window.ethereum) {
+          App.web3Provider = window.ethereum;
+          try {
+            // Request account access
+            await window.ethereum.enable();
+          } catch (error) {
+            // User denied account access...
+            console.error("User denied account access")
+          }
+        }
+        // Legacy dapp browsers...
+        else if (window.web3) {
+          App.web3Provider = window.web3.currentProvider;
+        }
+        // If no injected web3 instance is detected, fall back to Ganache
+        else {
+          App.web3Provider = new Web3.providers.HttpProvider('http://172.28.112.1:7545');
+        }
 
-    return App.initContract();
-  },
+        App.web3Provider = new Web3.providers.HttpProvider('http://172.28.112.1:7545');
+        web3 = new Web3(App.web3Provider);
+
+      return App.initContract();
+    },
 
   initContract: function() {
-    $.getJSON('Adoption.json', function(data) {
+    $.getJSON('QualityControl.json', function(data) {
+      console.log(data)
       // Get the necessary contract artifact file and instantiate it with @truffle/contract
-      var AdoptionArtifact = data;
-      App.contracts.Adoption = TruffleContract(AdoptionArtifact);
+      var QualityControlArtifact = data;
+      App.contracts.QualityControl = TruffleContract(QualityControlArtifact);
     
       // Set the provider for our contract
-      App.contracts.Adoption.setProvider(App.web3Provider);
+      App.contracts.QualityControl.setProvider(App.web3Provider);
     
       // Use our contract to retrieve and mark the adopted pets
-      return App.markAdopted();
+      return App.getTest();
     });
 
     return App.bindEvents();
   },
 
   bindEvents: function() {
-    $(document).on('click', '.btn-adopt', App.handleAdopt);
+    $(document).on('click', '#doorCheck', App.doorchanged);
+    $(document).on('click', '#cableCheck', App.cablechanged);
+    $(document).on('click', '#brakeCheck', App.brakechanged);
+    $(document).on('click', '#columnCheck', App.columnchanged);
+    $(document).on('click', '#batteryCheck', App.batterychanged);
+    $(document).on('click', '#submit', App.submitContrat);
   },
+  getTest: function() {
+//recupere les checkbox
 
-  markAdopted: function() {
-    var adoptionInstance;
 
-App.contracts.Adoption.deployed().then(function(instance) {
-  adoptionInstance = instance;
 
-  return adoptionInstance.getAdopters.call();
-}).then(function(adopters) {
-  for (i = 0; i < adopters.length; i++) {
-    if (adopters[i] !== '0x0000000000000000000000000000000000000000') {
-      $('.panel-pet').eq(i).find('button').text('Success').attr('disabled', true);
-    }
-  }
-}).catch(function(err) {
-  console.log(err.message);
-});
+    var QualityControlInstance;
+
+    App.contracts.QualityControl.deployed().then(function(instance) {
+      QualityControlInstance = instance;
+      var QualityControlInstance = instance;
+      
+          var address = QualityControlInstance.address.toString();
+
+          var datastring = {address: address, contract_type: "QualityControl"};
+
+          var data = JSON.stringify(datastring);
+          console.log(data);
+
+            $.ajax({
+              type: 'POST',
+              dataType: 'JSON',
+              headers: { 'content-type': 'application/json', "accept": "*/*", "Access-Control-Allow-Origin": "*" },
+              data: data,
+              url: 'https://rest-api-ag.azurewebsites.net/api/contracts',
+              success: function () {
+                  alert('YOUR CONTRACT HAS BEEN CREATED');
+              }
+    });
+      
+
+      return QualityControlInstance.getTest.call(x,y);
+    }).then(function(adopters) {
+    
+    }).catch(function(err) {
+      console.log(err.message);
+    });
+
+  },
+  submitContrat: function() {
+    //recupere les checkbox
+    
+    var QualityControlInstance;
+
+    App.contracts.QualityControl.deployed().then(function(instance) {
+      QualityControlInstance = instance;
+
+      return QualityControlInstance.getTest.call();
+    }).then(function(data) {
+      console.log(data)
+      // for (i = 0; i < adopters.length; i++) {
+      //   if (adopters[i] !== '0x0000000000000000000000000000000000000000') {
+      //     $('.panel-pet').eq(i).find('button').text('Success').attr('disabled', true);
+      //   }
+      // }
+    }).catch(function(err) {
+      console.log(err.message);
+    });
+
+
+
+
+  },
+  doorchanged: function(e){
+    App.doorV = e.target.checked;
+    
+    $("#submit").attr("disabled", !(App.doorV && App.cableV && App.batteryV && App.columnV && App.brakeV));
+  },
+  brakechanged: function(e){
+    App.brakeV = e.target.checked;
+    $("#submit").attr("disabled", !(App.doorV && App.cableV && App.batteryV && App.columnV && App.brakeV));
+  },
+  cablechanged: function(e){
+    App.cableV = e.target.checked;
+    $("#submit").attr("disabled", !(App.doorV && App.cableV && App.batteryV && App.columnV && App.brakeV));
+  },
+  batterychanged: function(e){
+    App.batteryV = e.target.checked;
+    $("#submit").attr("disabled", !(App.doorV && App.cableV && App.batteryV && App.columnV && App.brakeV));
+  },
+  columnchanged: function(e){
+    App.columnV = e.target.checked;
+    $("#submit").attr("disabled", !(App.doorV && App.cableV && App.batteryV && App.columnV && App.brakeV));
   },
 
   handleAdopt: function(event) {
@@ -96,6 +174,6 @@ web3.eth.getAccounts(function(error, accounts) {
 
 $(function() {
   $(window).load(function() {
-    App.init();
+    App.initWeb3();
   });
 });
