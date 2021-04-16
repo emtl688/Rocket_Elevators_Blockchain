a6fc82a893000251c27eb5b8788eae82bf9af97c
emtl688 <ericpmoran1@gmail.com>
2021-04-16 17:41:08 -0400

    Final Push

--- truffle-config.js
+++ truffle-config.js
@@ -1,3 +1,7 @@
+const HDWalletProvider = require("@truffle/hdwallet-provider");
+
+const mnemonic = "claw essay spoon blind early kite rural between usual tide express pigeon";
+
 module.exports = {
   // See <http://truffleframework.com/docs/advanced/configuration>
   // for more about customizing your Truffle configuration!
@@ -9,6 +13,12 @@ module.exports = {
     },
     develop: {
       port: 8545
+    },
+    ropsten: {
+      provider: function() {
+        return new HDWalletProvider(mnemonic, "https://ropsten.infura.io/v3/ee754186529c4304a7651c1f4290dd8a")
+      },
+      network_id: 3
     }
   }
 };
