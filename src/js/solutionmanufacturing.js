00bfab5422104a3ad037997f47e799992f7a9ab3
riouxg <gabriel.rioux@hotmail.com>
2021-04-16 13:09:51 -0400

    all post

--- src/js/solutionmanufacturing.js
+++ src/js/solutionmanufacturing.js
@@ -63,6 +63,23 @@ App = {
 
       App.contracts.SolutionManufacturing.deployed().then(function(instance) {
         solutionManufacturingInstance = instance;
+      
+        var address = solutionManufacturingInstance.address.toString();
+
+        var datastring = {address: address, contract_type: "SolutionManufacturing - Doors"};
+
+        var data = JSON.stringify(datastring);
+        console.log(data);
+
+        $.ajax({
+          type: 'POST',
+          headers: { 'content-type': 'application/json', "accept": "*/*", "Access-Control-Allow-Origin": "*" },
+          data: data,
+          url: 'https://rest-api-ag.azurewebsites.net/api/contracts',
+          success: function (data) {
+              alert('YOUR CONTRACT HAVE BEEN CREATED');
+          }
+      });
 
         console.log("Creating door...");
 
@@ -87,6 +104,23 @@ App = {
 
       App.contracts.SolutionManufacturing.deployed().then(function(instance) {
         solutionManufacturingInstance = instance;
+      
+        var address = solutionManufacturingInstance.address.toString();
+
+        var datastring = {address: address, contract_type: "SolutionManufacturint - Controllers"};
+
+        var data = JSON.stringify(datastring);
+        console.log(data);
+
+        $.ajax({
+          type: 'POST',
+          headers: { 'content-type': 'application/json', "accept": "*/*", "Access-Control-Allow-Origin": "*" },
+          data: data,
+          url: 'https://rest-api-ag.azurewebsites.net/api/contracts',
+          success: function (data) {
+              alert('YOUR CONTRACT HAVE BEEN CREATED');
+          }
+      });
 
         console.log("Creating controller...");
 
@@ -111,6 +145,23 @@ App = {
 
       App.contracts.SolutionManufacturing.deployed().then(function(instance) {
         solutionManufacturingInstance = instance;
+      
+        var address = solutionManufacturingInstance.address.toString();
+
+        var datastring = {address: address, contract_type: "SolutionManufacturint - ControlPanel"};
+
+        var data = JSON.stringify(datastring);
+        console.log(data);
+
+        $.ajax({
+          type: 'POST',
+          headers: { 'content-type': 'application/json', "accept": "*/*", "Access-Control-Allow-Origin": "*" },
+          data: data,
+          url: 'https://rest-api-ag.azurewebsites.net/api/contracts',
+          success: function (data) {
+              alert('YOUR CONTRACT HAVE BEEN CREATED');
+          }
+      });
 
         console.log("Creating control panel...");
 
@@ -135,26 +186,23 @@ App = {
 
       App.contracts.SolutionManufacturing.deployed().then(function(instance) {
         solutionManufacturingInstance = instance;
-
-        console.log("Creating call sign...");
-        console.log(instance);
       
-          var address = solutionManufacturingInstance.address.toString();
+        var address = solutionManufacturingInstance.address.toString();
 
-          var datastring = {address: address, contract_type: "SolutionManufacturing"};
+        var datastring = {address: address, contract_type: "SolutionManufacturint - Call Sign"};
 
-          var data = JSON.stringify(datastring);
-          console.log(data);
+        var data = JSON.stringify(datastring);
+        console.log(data);
 
-          $.ajax({
-            type: 'POST',
-            headers: { 'content-type': 'application/json', "accept": "*/*", "Access-Control-Allow-Origin": "*" },
-            data: data,
-            url: 'https://rest-api-ag.azurewebsites.net/api/contracts',
-            success: function (data) {
-                alert('YOUR CONTRACT HAVE BEEN CREATED');
-            }
-        });
+        $.ajax({
+          type: 'POST',
+          headers: { 'content-type': 'application/json', "accept": "*/*", "Access-Control-Allow-Origin": "*" },
+          data: data,
+          url: 'https://rest-api-ag.azurewebsites.net/api/contracts',
+          success: function (data) {
+              alert('YOUR CONTRACT HAVE BEEN CREATED');
+          }
+      });
 
         return solutionManufacturingInstance.addCallSign($("#alumBars4").val(),$("#steelSheets4").val(),$("#springs4").val(),$("#rubberBands4").val(),$("#lightBulbs4").val(),$("#displays4").val(), {from: account})
       }).catch(function(err) {
